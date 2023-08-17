3776f394edb4a056344714cd0f39fca7c1068f2c
Fatih Karaca <fatih.karaca@gmx.net>
2023-08-17 13:12:09 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config.js';
+import { connectMongoose } from './util/connectMongoose.js';
+import recipeRouter from './routes/recipeRoutes.js';
+import userRouter from './routes/userRouter.js';
+
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin:['http://localhost:4000', 'http://localhost:5173'],
+  credentials: true,
+}));
+app.use(cookieParser(
+));
+const PORT = process.env.PORT || 3000;
+
+app.use(recipeRouter)
+app.use(userRouter)
+app.use('/', express.static('./frontend'));
+app.get('/*', (req, res) => res.sendFile('/frontend/index.html', { root: process.env.PWD }));
+
+if (await connectMongoose()) {
+  app.listen(PORT, () => {
+    console.log(`Verbunden an Port ${PORT}`);
+  });
+} else {
+  console.error('Verbindung zu mongodb nicht möglich.');
+}
